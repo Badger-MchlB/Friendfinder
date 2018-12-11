e12138e2ad09bd344a09edab23ca71abf2c90cab
Badger-MchlB <boynton.mchl@gmail.com>
2018-12-11 03:09:21 -0500

    Port fix

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,4 +1,4 @@
-const expess = require('express');
+const express = require('express');
 const router = express.Router();
 const path = require('path');
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const htmlRouter = require('./app/routes/htmlRoutes');
 const apiRouter = require('./app/routes/apiRoutes');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
